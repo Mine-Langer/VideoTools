44fe4d5db26ef905790cef362806bdb79f0234eb
Mine-Langer <libing044@163.com>
2021-09-25 22:50:45 +0800

    .

--- Public/avsync.h
+++ Public/avsync.h
@@ -5,5 +5,15 @@
 class CAVSync
 {
 public:
-	void SetAudioClock(int64_t pts); // 设置当前音频主时钟
+	void SetAudioClock(int64_t pts) // 设置当前音频主时钟
+	{
+		AudioClock = pts;
+	}
+
+
+
+private:
+	volatile int64_t AudioClock = 0;
+	volatile int64_t VideoClock = 0;
+	volatile double ShowTime = 0;
 };
