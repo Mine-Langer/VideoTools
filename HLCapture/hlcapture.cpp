eed73ffd9ffde474382b2620c2d3df3df443d982
Mine-Langer <libing044@163.com>
2021-09-29 10:22:30 +0800

    .

--- HLCapture/hlcapture.cpp
+++ HLCapture/hlcapture.cpp
@@ -1,7 +1,15 @@
 #include "hlcapture.h"
+#include <QStandardPaths>
 
 HLCapture::HLCapture(QWidget *parent)
     : QWidget(parent)
 {
     ui.setupUi(this);
+
+    ui.radioScreenCap->setChecked(true);
+    ui.radioAudioSys->setChecked(true);
+    ui.radioHighQuality->setChecked(true);
+    ui.radioMP4->setChecked(true);
+    QString szDesktopPath = QStandardPaths::writableLocation(QStandardPaths::DesktopLocation);
+    ui.editOutputPath->setText(szDesktopPath);
 }
