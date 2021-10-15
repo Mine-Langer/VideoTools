9ca38221718682924b928dc5761b5f885fba6fc6
Mine-Langer <libing044@163.com>
2021-10-15 18:05:30 +0800

    ui

--- HLCapture/AVMainWnd.cpp
+++ HLCapture/AVMainWnd.cpp
@@ -4,8 +4,25 @@ AVMainWnd::AVMainWnd(QWidget *parent)
 	: QWidget(parent)
 {
 	ui.setupUi(this);
+
+	connect(ui.btnScreenRecording, SIGNAL(clicked()), this, SLOT(OnBtnMenuClicked()));
 }
 
 AVMainWnd::~AVMainWnd()
 {
+}
+
+void AVMainWnd::OnBtnMenuClicked()
+{
+	QPushButton* btnMenu = qobject_cast<QPushButton*>(sender());
+	QLayout* layout = ui.mainPageView->layout();
+	if (m_pCurrentWidget != nullptr) {
+		layout->removeWidget(m_pCurrentWidget);
+	}
+
+	if (btnMenu == ui.btnScreenRecording)
+	{
+		layout->addWidget(&m_capture);
+		m_pCurrentWidget = &m_capture;
+	}
 }
--- HLCapture/AVMainWnd.h
+++ HLCapture/AVMainWnd.h
@@ -2,6 +2,7 @@
 
 #include <QWidget>
 #include "ui_AVMainWnd.h"
+#include "hlcapture.h"
 
 class AVMainWnd : public QWidget
 {
@@ -11,6 +12,12 @@ public:
 	AVMainWnd(QWidget *parent = Q_NULLPTR);
 	~AVMainWnd();
 
+
+private slots:
+	void OnBtnMenuClicked();
+
 private:
 	Ui::AVMainWnd ui;
+	HLCapture m_capture;
+	QWidget* m_pCurrentWidget = nullptr;
 };
